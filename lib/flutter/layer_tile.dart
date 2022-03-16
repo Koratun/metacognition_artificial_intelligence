2ea6df258b3ff5fad3e82869111a11322b09c415
Kody <danmitch@uat.edu>
2022-03-15 22:04:17 -0700

    Added entrance animation to gridview's children

--- lib/flutter/layer_tile.dart
+++ lib/flutter/layer_tile.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 class LayerTile extends StatefulWidget {
   final int _i;
   final String _title;
+  final Animation<double> _entranceAnimation;
 
-  const LayerTile(this._i, this._title, {Key? key}) : super(key: key);
+  const LayerTile(this._i, this._title, this._entranceAnimation, {Key? key})
+      : super(key: key);
 
   @override
   State<LayerTile> createState() => _LayerTileState();
 }
 
 class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
+  late final AnimationController _hoverController = AnimationController(
     vsync: this,
     duration: const Duration(milliseconds: 100),
   );
@@ -19,7 +21,7 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
   late final Animation<double> _sizeAnimation = Tween<double>(
     begin: 0.0,
     end: 8.0,
-  ).animate(_controller);
+  ).animate(_hoverController);
 
   @override
   void initState() {
@@ -31,7 +33,7 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
 
   @override
   void dispose() {
-    _controller.dispose();
+    _hoverController.dispose();
     super.dispose();
   }
 
@@ -42,10 +44,10 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
       },
       child: MouseRegion(
         onEnter: (event) {
-          _controller.forward();
+          _hoverController.forward();
         },
         onExit: (event) {
-          _controller.reverse();
+          _hoverController.reverse();
         },
         // onHover: (event) {
         //   print("Hover");
@@ -59,31 +61,34 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return UnconstrainedBox(
       child: LimitedBox(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            _mouseDetector(Container(
-              decoration: BoxDecoration(
-                color: widget._i ~/ 3 < 4
-                    ? Colors.grey[100 * (widget._i ~/ 3 + 1)]
-                    : Colors.grey[100 * (widget._i ~/ 3 + 2)],
-                border: Border.all(color: Colors.lightBlueAccent[700]!),
-                borderRadius: BorderRadius.circular(8.0),
+        child: ScaleTransition(
+          scale: widget._entranceAnimation,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _mouseDetector(Container(
+                decoration: BoxDecoration(
+                  color: widget._i ~/ 3 < 4
+                      ? Colors.grey[100 * (widget._i ~/ 3 + 1)]
+                      : Colors.grey[100 * (widget._i ~/ 3 + 2)],
+                  border: Border.all(color: Colors.lightBlueAccent[700]!),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Icon(
+                  Icons.layers,
+                  size: 64 + _sizeAnimation.value,
+                  color: widget._i ~/ 3 < 4 ? Colors.black : Colors.white,
+                ),
+              )),
+              Text(
+                "${widget._title} ${widget._i}",
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white,
+                ),
               ),
-              child: Icon(
-                Icons.layers,
-                size: 64 + _sizeAnimation.value,
-                color: widget._i ~/ 3 < 4 ? Colors.black : Colors.white,
-              ),
-            )),
-            Text(
-              "${widget._title} ${widget._i}",
-              style: const TextStyle(
-                fontSize: 16.0,
-                color: Colors.white,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
