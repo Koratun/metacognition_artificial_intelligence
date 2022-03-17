c7c88266d6d7dcf73dff1438db1c89c325f71770
Kody <danmitch@uat.edu>
2022-03-17 14:18:08 -0700

    Created POC for dart-python communication

--- lib/flutter/main.dart
+++ lib/flutter/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'app_layout.dart';
+import 'pycontroller.dart';
 import 'selection_panel.dart';
 
 void main() {
@@ -17,86 +17,91 @@ class MainApp extends StatelessWidget {
       title: 'Metacognition Artificial Intelligence',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.black,
-          canvasColor: Colors.black,
-          textTheme: Typography.blackRedmond),
+        primaryColor: Colors.black,
+        canvasColor: Colors.black,
+        textTheme: Typography.blackRedmond,
+      ),
       home: Scaffold(
-        body: Stack(
-          children: [
-            Positioned.fill(
-              child: Container(
-                color: Colors.black,
-                child: Center(
-                  child: Text(
-                    'Creation',
-                    style: Theme.of(context).textTheme.headline4!.copyWith(
-                          color: Colors.white,
-                        ),
+        body: PyController(
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black,
+                  child: Center(
+                    child: Text(
+                      'Creation',
+                      style: Theme.of(context).textTheme.headline4!.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Positioned.fill(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const SelectionPanel(),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: SizedBox(
-                            width: 150,
-                            height: 40,
-                            child: Container(
-                              color: Colors.orange,
-                              child: Center(
-                                child: Text(
-                                  'Tools',
-                                  style: Theme.of(context).textTheme.headline6,
+              Positioned.fill(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SelectionPanel(),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Align(
+                            alignment: Alignment.topCenter,
+                            child: SizedBox(
+                              width: 150,
+                              height: 40,
+                              child: Container(
+                                color: Colors.orange,
+                                child: Center(
+                                  child: Text(
+                                    'Tools',
+                                    style:
+                                        Theme.of(context).textTheme.headline6,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomCenter,
-                          child: SizedBox(
-                            width: 800,
-                            height: 200,
-                            child: Container(
-                              color: Colors.green,
-                              child: Center(
-                                child: Text(
-                                  'Report',
-                                  style: Theme.of(context).textTheme.headline6,
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: SizedBox(
+                              width: 800,
+                              height: 200,
+                              child: Container(
+                                color: Colors.green,
+                                child: Center(
+                                  child: Text(
+                                    'Report',
+                                    style:
+                                        Theme.of(context).textTheme.headline6,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 128 * 3,
-                    height: 600,
-                    child: Container(
-                      color: Colors.yellow,
-                      child: Center(
-                        child: Text(
-                          'Dialogue',
-                          style: Theme.of(context).textTheme.headline6,
+                    SizedBox(
+                      width: 128 * 3,
+                      height: 600,
+                      child: Container(
+                        color: Colors.yellow,
+                        child: Center(
+                          child: Text(
+                            'Dialogue',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
