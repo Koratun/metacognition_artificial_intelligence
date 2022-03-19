eb03d0e036ee9ce49de4b8734c25af347b472830
Kody <danmitch@uat.edu>
2022-03-19 16:48:30 -0700

    Can drag the icons from the grid to wherever!

--- lib/flutter/layer_tile.dart
+++ lib/flutter/layer_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:metacognition_artificial_intelligence/flutter/mouse_hugger.dart';
 
 class LayerTile extends StatefulWidget {
   final int _i;
@@ -42,10 +43,12 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
   }
 
   Widget _mouseDetector(Widget child) {
-    return GestureDetector(
-      onTap: () {
-        print("Tapped");
-      },
+    return Listener(
+      onPointerDown: (event) => MouseHugger.of(context).setHugger(
+          LimitedBox(
+            child: _imageTile(hovering: true),
+          ),
+          event),
       child: MouseRegion(
         onEnter: (event) {
           _hoverController.forward();
@@ -53,12 +56,30 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
         onExit: (event) {
           _hoverController.reverse();
         },
-        // onHover: (event) {
-        //   print("Hover");
-        // },
         child: child,
       ),
     );
+  }
+
+  Widget _imageTile({bool hovering = false}) {
+    final Widget imageTile = Container(
+      decoration: BoxDecoration(
+        color: widget._i ~/ 3 < 4
+            ? Colors.grey[100 * (widget._i ~/ 3 + 1)]
+            : Colors.grey[100 * (widget._i ~/ 3 + 2)],
+        border: Border.all(color: Colors.lightBlueAccent[700]!),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Icon(
+        Icons.layers,
+        size: 64 + (hovering ? 0 : _sizeAnimation.value),
+        color: widget._i ~/ 3 < 4 ? Colors.black : Colors.white,
+      ),
+    );
+    if (hovering) {
+      return Opacity(opacity: 0.45, child: imageTile);
+    }
+    return imageTile;
   }
 
   @override
@@ -68,20 +89,7 @@ class _LayerTileState extends State<LayerTile> with TickerProviderStateMixin {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          _mouseDetector(Container(
-            decoration: BoxDecoration(
-              color: widget._i ~/ 3 < 4
-                  ? Colors.grey[100 * (widget._i ~/ 3 + 1)]
-                  : Colors.grey[100 * (widget._i ~/ 3 + 2)],
-              border: Border.all(color: Colors.lightBlueAccent[700]!),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Icon(
-              Icons.layers,
-              size: 64 + _sizeAnimation.value,
-              color: widget._i ~/ 3 < 4 ? Colors.black : Colors.white,
-            ),
-          )),
+          _mouseDetector(_imageTile()),
           Text(
             "${widget._title} ${widget._i}",
             style: const TextStyle(
