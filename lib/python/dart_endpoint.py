0627cef1da1dc38de45c2c1f7f32b486c654f542
Kody <danmitch@uat.edu>
2022-05-03 09:45:34 -0700

    Added layer package sorting and settings updates can now be selective instead of requiring all fields

--- lib/python/dart_endpoint.py
+++ lib/python/dart_endpoint.py
@@ -8,6 +8,7 @@ from uuid import UUID
 from pydantic import BaseModel, ValidationError, validator
 from lib.python.directed_acyclic_graph import LayerSettings, CompileException
 from python.directed_acyclic_graph import DagException, DirectedAcyclicGraph, Layer
+from pathlib import Path
 
 # When a new layer is created, add it to the list!
 from python.layers import (
@@ -20,6 +21,8 @@ layer_classes: dict[str, Type[Layer]] = {}
 # Loop backwards through globals() until we hit 'Layer' 
 # or whatever the first non-layer-subclass is
 
+layer_packages: dict[str, list[str]] = {}
+
 # We must wrap the globals call in a new dict because the very act
 # of creating this loop will alter the global variables and 
 # loops are not be able to iterate over changed iterators
@@ -28,11 +31,18 @@ for glob_mod_name, glob_mod in reversed(dict(globals()).items()):
     if glob_mod_name == 'input':
         breakflag = True
     if isinstance(glob_mod, type(input)):
+        mod_parent = Path(glob_mod.__file__).parent.name
+        if mod_parent == 'layers':
+            mod_parent = 'core'
         # This is a module!
         # Iterate through the module to find an attribute of type Layer
         for attr_name, attr in reversed(glob_mod.__dict__.items()):
             if isinstance(attr, type(Layer)):
                 layer_classes[attr_name] = attr
+                if package_list := layer_packages.get(mod_parent):
+                    package_list.append(attr_name)
+                else:
+                    layer_packages[mod_parent] = [attr_name]
                 break
 
     if breakflag:
@@ -68,6 +78,7 @@ def main():
     compile{}
     """
     write_back("Py Starting")
+    write_back(format_response(category_list=layer_packages))
 
     for line in fileinput.input():
         inp = line.rstrip()
@@ -118,13 +129,15 @@ class UpdateLayer(CreateLayer):
 
     @validator('settings', pre=True)
     def setting_fields_match(cls, v: dict, values: dict, **kwargs):
-        layer: Type[Layer] = values['layer']
+        layer: Type[Layer] = values.get('layer')
+        if not layer:
+            raise ValueError("Layer not provided")
         setting_schema = MetaSchema.parse_obj(layer.settings_validator.schema())
-        all_fields = sorted(list(setting_schema.properties.keys()))
-        given_settings_fields = sorted(v.keys())
-        if all_fields == given_settings_fields:
-            return v
-        raise ValueError("Fields do not match the given layer settings!")
+        all_fields = list(setting_schema.properties.keys())
+        for given_field in v.keys():
+            if given_field not in all_fields:
+                raise ValueError(f"{given_field} is not a valid setting field for: {layer.__name__}")
+        raise v
 
 
 class DeleteNode(BaseModel):
