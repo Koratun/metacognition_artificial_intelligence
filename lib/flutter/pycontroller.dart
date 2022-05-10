387dc556bd7273d47cb3540f917d29e02a6e487b
Kody <danmitch@uat.edu>
2022-05-09 20:50:53 -0700

    Started dart handling of input from python

--- lib/flutter/pycontroller.dart
+++ lib/flutter/pycontroller.dart
@@ -1,5 +1,15 @@
 import 'dart:io';
 import 'dart:convert';
+import 'response_schemas/response_type_enum.dart';
+import 'response_schemas/startup_response.dart';
+import 'response_schemas/compile_error_response.dart';
+import 'response_schemas/compile_error_disjointed_response.dart';
+import 'response_schemas/compile_error_settings_validation_response.dart';
+import 'response_schemas/compile_success_response.dart';
+import 'response_schemas/creation_response.dart';
+import 'response_schemas/graph_exception_response.dart';
+import 'response_schemas/success_fail_response.dart';
+import 'response_schemas/validation_error_response.dart';
 
 class PyController {
   Process? _python;
@@ -10,7 +20,7 @@ class PyController {
 
   void _init() async {
     _python = await Process.start(
-      "python",
+      ".venv\\Scripts\\python.exe",
       [".\\lib\\python\\dart_endpoint.py"],
       runInShell: true,
     );
@@ -33,9 +43,72 @@ class PyController {
   void pyInputHandler(String data) {
     // Separate the text preceding the first [ or {
     // and the rest of the text.
-    final String responseType =
-        data.substring(0, data.indexOf(RegExp(r'[[{]')));
-    final String responseData = data.substring(data.indexOf(RegExp(r'[[{]')));
+    final ResponseType responseType = ResponseType.values.firstWhere((e) =>
+        e.toString() ==
+        "ResponseType." + data.substring(0, data.indexOf(RegExp(r'[[{]'))));
+    final Map<String, dynamic> responseData =
+        json.decode(data.substring(data.indexOf(RegExp(r'[[{]'))));
+    switch (responseType) {
+      case ResponseType.startup:
+        {
+          final StartupResponse response =
+              StartupResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.successFail:
+        {
+          final SuccessFailResponse response =
+              SuccessFailResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.creation:
+        {
+          final CreationResponse response =
+              CreationResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.graphException:
+        {
+          final GraphExceptionResponse response =
+              GraphExceptionResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.validationError:
+        {
+          final ValidationErrorResponse response =
+              ValidationErrorResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.compileError:
+        {
+          final CompileErrorResponse response =
+              CompileErrorResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.compileErrorSettingsValidation:
+        {
+          final CompileErrorSettingsValidationResponse response =
+              CompileErrorSettingsValidationResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.compileErrorDisjointed:
+        {
+          final CompileErrorDisjointedResponse response =
+              CompileErrorDisjointedResponse.fromJson(responseData);
+        }
+        break;
+      case ResponseType.compileSuccess:
+        {
+          final CompileSuccessResponse response =
+              CompileSuccessResponse.fromJson(responseData);
+        }
+        break;
+      default:
+        {
+          print("Unknown response type: $responseType FIX IT!!!");
+        }
+        break;
+    }
   }
 
   void dispose() {
