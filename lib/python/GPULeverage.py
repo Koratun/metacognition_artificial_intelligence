3b3dbd1cfa592f96a90c20104c1331527c8dde54
Kody <danmitch@uat.edu>
2022-06-03 12:17:14 -0700

    Cleaning
    Co-authored-by: Brandon Carey <bracarey@users.noreply.github.com>

--- lib/python/GPULeverage.py
+++ lib/python/GPULeverage.py
@@ -6,7 +6,7 @@ import tkinter.messagebox
 import subprocess
 
 
-def programInstall(url, path):
+def program_install(url, path):
     top = tkinter.Tk()
     canvas1 = tkinter.Canvas(top)
     canvas1.create_text(200, 50, text="This program can benifit from GPU accleration. \n Would you like to install the additional program to leverage that?", fill="black", font=('Helvetica 8'))
@@ -15,7 +15,7 @@ def programInstall(url, path):
     def install():
         tkinter.messagebox.showinfo("Downloading File", "Downloading File")
         top.destroy()
-        if os.path.exists(path) == False:
+        if not os.path.exists(path):
             urllib.request.urlretrieve(url, path)
         os.startfile(path)
     #if user choses to skip the installation of cuda
@@ -29,18 +29,20 @@ def programInstall(url, path):
     B2.pack()
     top.mainloop()
 
-#Creates the file in the users folder where the program is located.
-simp_path = 'NvidiaCUDA.exe'
-abs_path = os.path.abspath(simp_path)
-try:
-    subprocess.check_output('nvidia-smi')
-    if platform.system() == "Windows":
-        #CUDA Windows URL is the same for both Windows 10 and Windows 11
-        winUrl = 'https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_516.01_windows.exe'
-        programInstall(winUrl, abs_path)
-    #for future use. lines of code needed to run cuda install on linux. 
-    #elif platform.system() == "Linux":
-    #    runscript = 'wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run \n sudo sh cuda_11.7.0_515.43.04_linux.run'
-    #    os.system(runscript)
-except Exception: # this command not being found can raise quite a few different errors depending on the configuration
-    print('No Nvidia GPU in system!')
+
+def try_install_cuda():
+    #Creates the file in the users folder where the program is located.
+    simp_path = 'NvidiaCUDA.exe'
+    abs_path = os.path.abspath(simp_path)
+    try:
+        subprocess.check_output('nvidia-smi')
+        if platform.system() == "Windows":
+            #CUDA Windows URL is the same for both Windows 10 and Windows 11
+            winUrl = 'https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_516.01_windows.exe'
+            program_install(winUrl, abs_path)
+        #for future use. lines of code needed to run cuda install on linux. 
+        #elif platform.system() == "Linux":
+        #    runscript = 'wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run \n sudo sh cuda_11.7.0_515.43.04_linux.run'
+        #    os.system(runscript)
+    except Exception: # this command not being found can raise quite a few different errors depending on the configuration
+        print('No Nvidia GPU in system!')
