55e844497d66870d0dc6de4d18893690f21e53cc
Kody <danmitch@uat.edu>
2022-06-03 16:05:33 -0700

    Prep for datasources class

---
+++ lib/python/datasources.py
@@ -0,0 +1,11 @@
+from keras.datasets import (
+    cifar10,          # image class
+    cifar100,         # image class
+    reuters,          # topic class
+    imdb,             # sentiment class
+    boston_housing,   # price regression
+    mnist,            # image class
+    fashion_mnist     # image class
+)
+
+
