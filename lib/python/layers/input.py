86137a45035c499caa3d6db64e8b747c348ee83f
Kody <danmitch@uat.edu>
2022-06-04 17:56:50 -0700

    conform with new changes

--- lib/python/layers/input.py
+++ lib/python/layers/input.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from python.layers.layers import Dtype
+from python.layers.utils import Dtype
 from python.directed_acyclic_graph import NamedLayerSettings, Layer
 from pydantic import StrictInt
 
