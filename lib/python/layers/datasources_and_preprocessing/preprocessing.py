f991183e8a9c39c47c99ed74b7d92ac6dce5cb37
Kody <danmitch@uat.edu>
2022-06-06 19:47:34 -0700

    Added one hot encoding layer

--- lib/python/layers/datasources_and_preprocessing/preprocessing.py
+++ lib/python/layers/datasources_and_preprocessing/preprocessing.py
@@ -1,11 +1,19 @@
-from pydantic import ValidationError
+from pydantic import ValidationError, validator
 from python.directed_acyclic_graph import Layer, LayerSettings, DagNode, CompileException, CompileErrorReason
 from python.layers.datasources_and_preprocessing.datasources import DatasetVars, KerasDatasource
 from typing import Literal
 
 
 class PreprocessingLayer(Layer):
-    def get_datasetvars(self, node: DagNode) -> DatasetVars:
+    def __init__(self):
+        super().__init__()
+        self.datasource: KerasDatasource = None
+
+    def get_datasource(self, node: DagNode):
+        """
+        Populates the self.datasource variable by accessing the upstream node
+        which must be either a Preprocessing Layer or a Datasource.
+        """
         if self.constructed:
             raise CompileException({
                 'node_id': str(node.id), 
@@ -13,8 +21,10 @@ class PreprocessingLayer(Layer):
                 'errors': "This preprocessing node has already been constructed, you cannot construct a model twice."
             })
         upstream_layer = node.upstream_nodes[0].layer
-        if upstream_layer in (PreprocessingLayer, KerasDatasource):
-            return upstream_layer.dataset
+        if upstream_layer is PreprocessingLayer:
+            self.datasource = upstream_layer.datasource
+        elif upstream_layer is KerasDatasource:
+            self.datasource = upstream_layer
         else:
             raise CompileException({
                 'node_id': str(node.id), 
@@ -39,17 +49,56 @@ class MapRangeSettings(InputOrOutputSetting):
 class MapRange(PreprocessingLayer):
     settings_validator = MapRangeSettings
 
-
     def generate_code_line(self, node_being_built: DagNode) -> str:
-        dataset = self.get_datasetvars(node_being_built)
+        self.get_datasource(node_being_built)
 
         try:
             ranges: MapRangeSettings = self.settings_validator(**self.settings_data)
+            dataset: DatasetVars = self.datasource.dataset
             lines = "\n".join(
                 f"{dataset[i][ranges.io]} = ({dataset[i][ranges.io]} - {ranges.old_range_min}) / ({ranges.old_range_max} - {ranges.old_range_min}) "
                 f"* ({ranges.new_range_max} - {ranges.new_range_min}) + {ranges.new_range_min}"
                 for i in range(len(dataset))
             )
+            self.constructed = True
+            return lines
+        except ValidationError as e:
+            raise CompileException({
+                'node_id': str(node_being_built.id), 
+                'reason': CompileErrorReason.SETTINGS_VALIDATION.camel(), 
+                'errors': e.errors()
+            })
+
+
+class OneHotSettings(LayerSettings):
+    n_classes: int
+
+    @validator('n_classes')
+    def classification_prob(cls, v):
+        if v > 2:
+            return v
+        elif v == 2:
+            raise ValueError("One Hot encoding not recommmended for Binary classification problem")
+        else:
+            raise ValueError("Invalid number")
+
+
+class OneHotEncode(PreprocessingLayer):
+    settings_validator = OneHotSettings
+
+    def generate_code_line(self, node_being_built: DagNode) -> str:
+        self.get_datasource(node_being_built)
+
+        try:
+            n_classes: int = self.settings_validator(**self.settings_data).n_classes
+
+            lines = "def one_hot_encode(y_data):\n"
+            lines += f"\tencoded_y = np.zeros((len(y_data), {n_classes}))\n"
+            lines += "\tfor i, y in enumerate(y_data):\n"
+            lines += "\t\tencoded_y[i][y] = 1\n"
+            lines += "\treturn encoded_y\n\n"
+            lines += f"{self.datasource.dataset.train.y} = one_hot_encode({self.datasource.dataset.train.y})\n\n"
+
             self.constructed = True
             return lines
         except ValidationError as e:
