c29ede907ef654c06c31cf253897cfa13c14a44f
Kody <danmitch@uat.edu>
2022-07-01 15:26:50 -0700

    Created node connections and reworked the canvas state

--- lib/flutter/creation_canvas.dart
+++ lib/flutter/creation_canvas.dart
@@ -1,105 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'selection_panel.dart';
 import 'layer_tile.dart';
+import 'node_connection.dart';
 import 'pycontroller.dart';
+
 import 'schemas/command_type_enum.dart';
 import 'schemas/create_layer.dart';
-import 'schemas/schema.dart';
+import 'schemas/creation_response.dart';
 
-class CreationCanvas extends StatefulWidget {
+class CreationCanvas extends StatelessWidget {
   const CreationCanvas({Key? key}) : super(key: key);
 
   @override
-  State<CreationCanvas> createState() => _CreationCanvasState();
+  Widget build(BuildContext context) {
+    return Consumer<CreationCanvasState>(
+      builder: (context, canvasState, child) {
+        return DragTarget<LayerTile>(
+          builder: (context, candidateData, rejectedData) {
+            return Container(
+              color: Theme.of(context).canvasColor,
+              child: CustomMultiChildLayout(
+                delegate: CreationCanvasDelegate(
+                  changeNotifier: canvasState,
+                  positions: canvasState.positions,
+                  conns: canvasState.conns,
+                ),
+                children: [
+                  for (var pair in canvasState.conns.entries)
+                    LayoutId(
+                      id: pair.key,
+                      child: pair.value,
+                    ),
+                  for (var pair in canvasState.tiles.entries)
+                    LayoutId(
+                      id: pair.key,
+                      child: pair.value,
+                    )
+                ],
+              ),
+            );
+          },
+          onAcceptWithDetails: (DragTargetDetails details) =>
+              canvasState.addTile(details.data, details.offset),
+        );
+      },
+    );
+  }
 }
 
-class _CreationCanvasState extends State<CreationCanvas>
-    with TickerProviderStateMixin, ChangeNotifier {
-  final List<LayerTile> tiles = <LayerTile>[];
-  final List<Offset> positions = <Offset>[];
+class CreationCanvasState extends ChangeNotifier {
+  final TickerProvider _ticker;
+  final Map<String, LayerTile> tiles = {};
+  final Map<String, Offset> positions = {};
+  final Map<String, NodeConnection> conns = {};
+
+  CreationCanvasState(this._ticker);
 
   void addTile(LayerTile layerTile, Offset pos) {
-    final AnimationController _entranceController = AnimationController(
-      vsync: this,
+    final AnimationController entranceController = AnimationController(
+      vsync: _ticker,
       duration: const Duration(seconds: 1),
     );
 
-    final Animation<double> _steadyFall = CurvedAnimation(
-      parent: _entranceController,
+    final Animation<double> steadyFall = CurvedAnimation(
+      parent: entranceController,
       curve: Curves.easeInCirc,
     );
 
-    final Animation<double> _entranceAnimation =
-        Tween(begin: 16.0, end: 0.0).animate(_steadyFall);
+    final Animation<double> entranceAnimation =
+        Tween(begin: 16.0, end: 0.0).animate(steadyFall);
 
-    _entranceController.forward();
+    entranceController.forward();
 
     LayerTile newTile = LayerTile.canvasChild(
       layerTile.i,
       layerTile.category,
-      _entranceAnimation,
-      _entranceController,
+      entranceAnimation,
+      entranceController,
       changeNotifyCallback: notifyListeners,
       backgroundColor: categoryColors[layerTile.category],
       foregroundColor: layerTileAssetData[layerTile.name]?['color'],
       symbol: layerTileAssetData[layerTile.name]?['symbol'],
       name: layerTile.name,
-      messageHandler:
-          ValueNotifier<RequestResponseSchema>(RequestResponseSchema()),
     );
-    tiles.add(newTile);
-    positions.add(pos);
     if (newTile.name != null) {
       PyController.request(
         CommandType.create,
-        (response) => newTile.messageHandler!.value = response,
+        (response) {
+          if (response is CreationResponse) {
+            String id = response.nodeId;
+            tiles[id] = newTile;
+            positions[id] = pos;
+            newTile.messageHandler!.value = response;
+            notifyListeners();
+          }
+        },
         data: CreateLayer(newTile.name!),
       );
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return DragTarget<LayerTile>(
-      builder: (context, candidateData, rejectedData) {
-        return Container(
-          child: CustomMultiChildLayout(
-            delegate: CreationCanvasDelegate(
-                changeNotifier: this, positions: positions),
-            children: [
-              for (var i = 0; i < tiles.length; i++)
-                LayoutId(
-                  id: i,
-                  child: tiles[i],
-                )
-            ],
-          ),
-          color: Theme.of(context).canvasColor,
-        );
-      },
-      onAcceptWithDetails: (DragTargetDetails details) =>
-          setState(() => addTile(details.data, details.offset)),
-    );
+  void startConnection(String nodeId, Offset pos) {
+    conns[nodeId + ":"] = NodeConnection(pos);
+  }
+
+  void updateConnection({
+    required String startId,
+    String endId = "",
+    required Offset endPos,
+    Offset? startPos,
+  }) {
+    conns[startId + ":" + endId]?.end = endPos;
+    if (startPos != null && endId != "") {
+      conns[startId + ":" + endId]?.start = startPos;
+    }
+    notifyListeners();
+  }
+
+  void cancelConnection(String id) {
+    conns.remove(id + ":");
+    notifyListeners();
+  }
+
+  void connectNodes(String startId, String endId, Offset endPos) {
+    conns[startId + ":" + endId] = conns.remove(startId + ":")!..end = endPos;
+    notifyListeners();
   }
 }
 
 class CreationCanvasDelegate extends MultiChildLayoutDelegate {
-  final List<Offset> positions;
+  final Map<String, Offset> positions;
+  final Map<String, NodeConnection> conns;
 
-  CreationCanvasDelegate({required changeNotifier, required this.positions})
-      : super(relayout: changeNotifier);
+  CreationCanvasDelegate({
+    required Listenable changeNotifier,
+    required this.positions,
+    required this.conns,
+  }) : super(relayout: changeNotifier);
 
   @override
   void performLayout(Size size) {
-    for (var tileID = 0; tileID < positions.length; tileID++) {
+    for (var pair in conns.entries) {
+      if (hasChild(pair.key)) {
+        layoutChild(pair.key, const BoxConstraints.tightForFinite());
+        Offset pos = pair.value.quadrantSwitch(
+          bottomRight: () => pair.value.start.translate(-3, -3),
+          bottomLeft: () => Offset(
+            pair.value.end.dx - 3,
+            pair.value.start.dy - 3,
+          ),
+          topLeft: () => pair.value.end.translate(-3, -3),
+          topRight: () => Offset(
+            pair.value.start.dx - 3,
+            pair.value.end.dy - 3,
+          ),
+        );
+        positionChild(pair.key, pos);
+      }
+    }
+    for (var pair in positions.entries) {
       Size tileSize = Size.zero;
-      if (hasChild(tileID)) {
-        tileSize = layoutChild(tileID, const BoxConstraints.tightForFinite());
+      if (hasChild(pair.key)) {
+        tileSize = layoutChild(pair.key, const BoxConstraints.tightForFinite());
         positionChild(
-            tileID,
-            positions[tileID]
-                .translate(tileSize.width / -2, tileSize.height / -2));
+          pair.key,
+          pair.value.translate(tileSize.width / -2, tileSize.height / -2),
+        );
       }
     }
   }
@@ -109,8 +177,8 @@ class CreationCanvasDelegate extends MultiChildLayoutDelegate {
     if (positions.length != oldDelegate.positions.length) {
       return true;
     }
-    for (var i = 0; i < positions.length; i++) {
-      if (positions[i] != oldDelegate.positions[i]) {
+    for (var pair in positions.entries) {
+      if (pair.value != oldDelegate.positions[pair.key]) {
         return true;
       }
     }
