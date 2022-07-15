9aaa85263acb148747a0b4324e08e38e1adfaba3
Daniel <doctordalekcon@gmail.com>
2022-07-14 17:01:08 -0700

    started dialogue panel

---
+++ lib/flutter/dialogue_panel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DialoguePanel extends StatelessWidget {
+  const DialoguePanel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
+
+class DialoguePanelState extends ChangeNotifier {}
